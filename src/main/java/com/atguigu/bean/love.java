80928d98217f5eea5159f6206d868d8329ee16da
王伟东 <963645446@qq.com>
2020-09-26 21:03:43 +0800

    在IDEA中测试

---
+++ src/main/java/com/atguigu/bean/love.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author shkstart
+ * @create 2020-09-26 20:44
+ */
+public class love {
+    public static void main(String[] args) {
+        System.out.println("今天是个神圣的日子。。。。。");
+    }
+}
